355681e3da381e38923687c7c5e8febedeb70b06
Artur Siery <artur.siery@exaring.de>
2024-05-03 14:56:03 +0200

    cmd-s to save (uses item2 mapping)

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,3 +19,10 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+-- keymapings
+-- vim.api.nvim_set_keymap('n', '<D-s>', ':w<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-s>', ':w<CR>', { noremap = true, silent = true })
+
+
