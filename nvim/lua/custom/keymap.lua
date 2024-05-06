25dbbee1d775fd42b6da68f9142e977304ac1daf
Artur Siery <artur.siery@exaring.de>
2024-05-06 11:41:31 +0200

    wip

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -2,4 +2,4 @@
 vim.api.nvim_set_keymap('n', '<M-s>', ':w<CR>', { noremap = true, silent = true })
 
 -- Set ladder g D to use telescope
-vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Telescope lsp_definitions<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Telescope lsp_definitions<CR>', { noremap = true, silent = true })
