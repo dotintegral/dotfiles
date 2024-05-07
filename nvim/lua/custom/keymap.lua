5648204ddb54c5da39962b90688633ee19711745
Artur Siery <artur.siery@exaring.de>
2024-05-07 17:29:43 +0200

    binding for code actions

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -6,3 +6,6 @@ vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Telescope lsp_definitions<CR>',
 
 -- Set leader g r to use telescope
 vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Telescope lsp_references<CR>', { noremap = true, silent = true })
+
+-- Set leader . to use code actions
+vim.api.nvim_set_keymap('n', '<leader>.', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
