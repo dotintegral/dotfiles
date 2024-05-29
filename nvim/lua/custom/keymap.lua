a71c796b40fc2b555097bf393d6f798603b73890
Artur Siery <artur.siery@exaring.de>
2024-05-29 08:56:03 +0200

    add desc

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -1,3 +1,4 @@
+-- local map = vim.keymap.map
 -- Set CMD-S to save
 -- vim.api.nvim_set_keymap('n', '<M-s>', ':lua vim.lsp.buf.format()<CR>:EslintFixAll<CR>:w<CR>', { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', '<M-s>',
@@ -8,13 +9,13 @@ vim.api.nvim_set_keymap('n', '<M-s>', ':PrettierCLI<CR>:w<CR>',
   { noremap = true, silent = true })
 
 -- Set leader g D to use telescope
-vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Telescope lsp_definitions<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Telescope lsp_definitions<CR>', { noremap = true, silent = true, desc = 'Go to Definition' })
 
 -- Set leader g r to use telescope
-vim.api.nvim_set_keymap('n', '<leader>gr', '<cmd>Telescope lsp_references<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gr', '<cmd>Telescope lsp_references<CR>', { noremap = true, silent = true, desc = 'Go to References' })
 
 -- Set leader . to use code actions
-vim.api.nvim_set_keymap('n', '<leader>.', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>.', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true, desc = 'Code Actions' })
 
 -- Set leader b to open buffers
 vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
@@ -24,7 +25,7 @@ vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>Telescope buffers<CR>', { norema
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 
-vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true, desc = 'Accept Copilot' })
 
 -- C-q in telescope sends selected files to qflist and opens qflist 
 vim.api.nvim_set_keymap('n', '<C-q>', '<cmd>lua require("telescope.actions").send_to_qflist()<CR><cmd>lua require("telescope.actions").open_qflist()<CR>', { noremap = true, silent = true })
@@ -36,3 +37,8 @@ vim.api.nvim_set_keymap('i', '<C-Q>', '<cmd>lua require("telescope.actions").sen
 
 -- leader g y shows neoclip in telescope
 vim.api.nvim_set_keymap('n', '<leader>gy', '<cmd>lua require("telescope").extensions.neoclip.default()<CR>', { noremap = true, silent = true })
+
+--leader g e goes to next diagnostic, severity: error or warning, uses vim.diagnostic
+vim.api.nvim_set_keymap('n', '<leader>ge', '<cmd>lua vim.diagnostic.goto_next({severity = { vim.diagnostic.severity.WARN, vim.diagnostic.severity.ERROR }})<CR>', { noremap = true, silent = true })
+
+
