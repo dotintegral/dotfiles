9c7e559c9cc8e0501bf7aa07a6b8fa9641b8029d
Artur Siery <artur.siery@exaring.de>
2025-03-13 07:03:16 +0100

    fix visual selection color

--- nvim/lua/custom/settings.lua
+++ nvim/lua/custom/settings.lua
@@ -5,6 +5,12 @@ vim.opt.scrolloff = 10
 vim.o.cursorline = true
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#425047" })
 
+-- Set the color of the visual selection
+vim.cmd([[
+  highlight Visual ctermfg=NONE ctermbg=DarkGrey gui=NONE guifg=NONE guibg=#5c5050
+]])
+
 -- Enable loading project specific settings
 vim.opt.exrc = true
+
 
