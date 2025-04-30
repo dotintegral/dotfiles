a31fdc20dd65e53fa5284ec642e3b8cf3d5ada5f
Artur Siery <artur.siery@exaring.de>
2025-04-30 11:12:15 +0200

    add copilotchat

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -134,6 +134,19 @@ local plugins = {
   {
     "ggandor/leap.nvim",
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    lazy = false,
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
 
 return plugins
