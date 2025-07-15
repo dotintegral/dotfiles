26d1b164cc643aad74c9d3f99584305dcf2da4d1
Artur Siery <artur.siery@exaring.de>
2025-07-15 15:23:01 +0200

    add Eslint/Prettier and some keybindings

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+function SaveAndFormat()
+  vim.cmd("w")
+end
+
+-- Set CMD-S to save
+vim.api.nvim_set_keymap("n", "<M-s>", "<cmd>lua SaveAndFormat()<CR>", {
+  noremap = true,
+  silent = true,
+})
+
+-- Set leader . to use code actions
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>.",
+  "<cmd>lua vim.lsp.buf.code_action()<CR>",
+  { noremap = true, silent = true, desc = "Code Actions" }
+)
