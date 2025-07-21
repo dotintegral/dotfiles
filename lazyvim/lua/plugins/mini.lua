8a4e459204674b17a3ea7f1d1b1808c16dd26919
Artur Siery <artur.siery@exaring.de>
2025-07-21 15:39:39 +0200

    add mini comment

---
+++ lazyvim/lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.comment",
+  version = false,
+  config = function()
+    require("mini.comment").setup({
+      mappings = {
+        comment_line = "<leader>/",
+        comment_visual = "<leader>/",
+      },
+    })
+  end,
+}
