e337b0418ccbc7925442e416fd642c34ea7e9351
Artur Siery <artur.siery@exaring.de>
2025-11-28 11:56:00 +0100

    add better colors

--- wezterm.lua
+++ wezterm.lua
@@ -3,10 +3,34 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local wallpaper = (os.getenv("HOME") or "") .. "/style/wallpapers/terminal-background.png"
 
-config.color_scheme = "Everforest Dark Hard (Gogh)"
 config.font = wezterm.font("Fira Code")
 
 config.font_size = 11.5
+
+config.colors = {
+	foreground = "#d3c6aa",
+	background = "#252d34",
+	ansi = {
+		"#475258", -- color0
+		"#e67e80", -- color1
+		"#a7c080", -- color2
+		"#dbbc7f", -- color3
+		"#7fbbb3", -- color4
+		"#d699b6", -- color5
+		"#83c092", -- color6
+		"#d3c6aa", -- color7
+	},
+	brights = {
+		"#a7afa1", -- color8
+		"#e56058", -- color9
+		"#90a030", -- color10
+		"#d5a236", -- color11
+		"#5392c0", -- color12
+		"#d06fb6", -- color13
+		"#58a47e", -- color14
+		"#eeebde", -- color15
+	},
+}
 
 config.window_background_opacity = 1.0
 config.text_background_opacity = 1.0
